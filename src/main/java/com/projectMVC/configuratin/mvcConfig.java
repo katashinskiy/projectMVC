df0c8c0f25972eeefc0d8839ac97879646388cda
violance228 <sviripa1234@gmail.com>
2018-07-19 11:51:52 +0300

    3 lesson

---
+++ src/main/java/com/projectMVC/configuratin/mvcConfig.java
@@ -0,0 +1,15 @@
+package com.projectMVC.configuratin;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class mvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
